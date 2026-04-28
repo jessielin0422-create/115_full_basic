7162c9dec26276e677144d919fd3f2e34650c86b
jessie <jessielin0422@gmail.com>
2026-04-28 15:35:46 +0800

    反轉陣列未完成

--- 13-pra09.php
+++ 13-pra09.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>五百年內的閏年</title>
 </head>
+
 <body>
 
-<3>找出五百年內的閏年</h3>
+     <3>找出五百年內的閏年</h3>
 
 
-<ul>
-     <li>請依照閏年公式找出五百年內的閏年</li>
-     <li>使用陣列來儲存閏年</li>
-     <li>使用迴圈來印出閏年</li>
-</ul>
- <?php
+          <ul>
+               <li>請依照閏年公式找出五百年內的閏年</li>
+               <li>使用陣列來儲存閏年</li>
+               <li>使用迴圈來印出閏年</li>
+          </ul>
+          <?php
 $year = 2026;
 
 $yaers=[];
@@ -41,6 +43,7 @@ foreach($years as $idx => $leap_year){
     
 
 ?>
-     
+
 </body>
+
 </html>
