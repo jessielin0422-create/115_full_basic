a093cc29a6bd381c19e579fd6154cf1fe65c4c21
jessie <jessielin0422@gmail.com>
2026-04-28 14:29:30 +0800

    未完成

---
+++ 14-pra10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=
+     , initial-scale=1.0">
+     <title>西元年 <=> 天干地支年</title>
+</head>
+<body>
+     <h3>已知西元1024年為甲子年，請設計一支程式，可以接受任一西元年份，輸出對應的天干地支的年別。(利用迴圈)</h3>
+
+<ul>
+     <li>天干：甲乙丙丁戊己庚辛壬癸</li>
+     <li>地支：子丑寅卯辰巳午未申酉戌亥</li>
+     <li>天干地支配對：甲子、乙丑、丙寅….甲戌、乙亥、丙子….</li>
+</ul>
+
+
+</body>
+</html>
